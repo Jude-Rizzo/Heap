e8b69bb3c66d3fb1bae877433f63d2d9e03b5ab7
Jude Rizzo <jrizzo00@stuy.edu>
2019-05-01 14:24:44 -0400

    now i have checkchildren, working on pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -1,8 +1,19 @@
 public class MyHeap{
   //swap method for later convinience
-  private static void swap(int[] data, int a, int b) {
-		int sto = data[a];
-		data[a] = data[b];
-		data[b] = sto;
+  private static void swap(int[] data, int x, int y) {
+		int sto = data[x];
+		data[x] = data[y];
+		data[y] = sto;
 	}
+
+  }
+
+
+//Doing the pushdown method
+
+private static int checkChildren(int[] data, int size, int index){
+  right = 2 * index + 2
+  if(right == size) return 1;
+  if(right < size) return 2;
+  if(right > size) return 0;
 }
